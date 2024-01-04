b7c5b6095e653a2fcd49c3ca8228de5c62a72653
subtitle1 <subtitle1@naver.com>
2024-01-04 16:08:21 +0900

    start cj crawling

--- 4. CJ_live.py
+++ 4. CJ_live.py
@@ -2,5 +2,14 @@
 from common.web_imports import *
 from common.common_functions import get_next_date, initialize_dates
 
-# 20240105
-url = 'https://display.cjonstyle.com/p/homeTab/main?BSCCN1=SA_NV_B_PC_7&BSCPN=CJmall&BSPRG=NVBRNO&hmtabMenuId=002409#bdDt%3A20240105'
+today, current_date, end_date = initialize_dates(3, 8)
+df = initialize_columns()
+
+while current_date <= end_date:
+    given_url = 'https://display.cjonstyle.com/p/homeTab/main?BSCCN1=SA_NV_B_PC_7&BSCPN=CJmall&BSPRG=NVBRNO&hmtabMenuId=002409#bdDt%3A'
+    driver = initialize_page(current_date, given_url)
+
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    items = soup.find_all('div', attrs={'class': 'schedule_prod'})
+
+    print(items)

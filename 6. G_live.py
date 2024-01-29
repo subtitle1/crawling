4fe5c3d4e5695b0b1ffe45f1e9667c3fa75dc042
subtitle1 <subtitle1@naver.com>
2024-01-29 17:00:13 +0900

    G live crawling

--- 6. G_live.py
+++ 6. G_live.py
@@ -1,5 +1,8 @@
 from common.mobile_imports import *
-from common.common_functions import get_next_date, initialize_dates
+from datetime import datetime
+
+df = initialize_columns(date_format=True)
+today = datetime.now()
 
 url = 'https://m.gmarket.co.kr/n/live/_next/data/bMXsOgO3o7Wlc3Y8T3dKE/n/live/schedule.json'
 requestData = requests.get(url)
@@ -7,10 +10,41 @@ jsonData = None
 
 if requestData.status_code == 200 :
     jsonData = requestData.json()
-    productList = jsonData['pageProps']['initialStates']['schedule']['liveCatalogs']
+    liveCatalogs = jsonData.get('pageProps', {}).get('initialStates', {}).get('schedule', {}).get('liveCatalogs', [])
+    # liveCatalogs = jsonData['pageProps']['initialStates']['schedule']['liveCatalogs']
 
-    print(productList)
+input_format = "%Y-%m-%dT%H:%M:%S"
+output_format = "%m/%d %H시 %M분"
 
-    # liveURL = landingUrl
-    # imageUrl, broadcastTitle, brand_logo = seller['logoImage'], brand = seller['name']
-    # start_live_time = broadcastStartDate, live_end_time = broadcastEndDate
+while True :
+    category = "라이브특가정보"
+    sub_category = "G live 홈쇼핑"
+
+    try:
+        for item in liveCatalogs:
+            live_list = item.get('lives', [])
+
+            for i in live_list:
+                live_url = i['broadcastSeq']
+                brand = i['seller']['name']
+                brand_logo = i['seller']['logoImage']
+
+                title = i['broadcastTitle']
+                live_url = i['landingUrl']
+                image_url = i['imageUrl']
+
+                start_live_time_str = i['broadcastStartDate']
+                end_live_time_str = i['broadcastEndDate']
+
+                start_live_time = datetime.strptime(start_live_time_str, input_format).strftime(output_format)
+                end_live_time = datetime.strptime(end_live_time_str, input_format).strftime(output_format)
+
+                # print(brand, brand_logo, start_live_time, live_url)
+                df = pd.concat([df, pd.DataFrame([[category, sub_category, title, brand, '', '', category, '', start_live_time, end_live_time, live_url, brand_logo, image_url]], columns=df.columns)], ignore_index=True)
+
+    except Exception as e:
+        print(f"오류가 발생했습니다: {e}")
+        pass
+
+    address = "C:\\work\\"
+    df.to_excel(excel_writer = address + today.strftime("%Y%m%d") + '_G live.xlsx')
